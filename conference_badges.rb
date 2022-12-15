5c20d2640a317ffde6e7d61bd0aafafd9c3b5c80
Nick Landas <nick.landas@gmail.com>
2022-12-14 18:45:31 -0600

    commit tree c79a68c2ae2e443fe773bffa9b77ea6581bc265b

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+    p "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    arr.map {|name| badge_maker(name) }
+end
+def assign_rooms (arr)
+    arr.map.with_index(1) {|name, index|"Hello, #{name}! You'll be assigned to room #{index}!"}
+end
+
+def printer(speakers)
+    batch_badge_creator(speakers).each {|speaker| puts speaker}
+    assign_rooms(speakers).each {|speaker| puts speaker}
+end
